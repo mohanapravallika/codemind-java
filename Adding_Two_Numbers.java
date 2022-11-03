0e82686b08c3cb2c215d50026285422d04a82bfa
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-11-03 20:38:54 +0530

    import java.util.Scanner; import java.lang.Math; class Circle {     public static void main(String args[])     {         int a,b;         int T;         Scanner sc = new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         T=a+b;         System.out.println(T);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Circle
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        int T;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        T=a+b;
+        System.out.println(T);
+    }
+}
