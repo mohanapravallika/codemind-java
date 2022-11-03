50a83cc527fd92854ac542facacee1ce7c0eec00
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-11-03 20:55:13 +0530

    import java.util.Scanner; import java.lang.Math; class Mod {     public static void main(String args[])     {         int a,b;         int t;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         t=a/b;         System.out.println(t);     } }

---
+++ Division.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Mod
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        int t;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        t=a/b;
+        System.out.println(t);
+    }
+}
