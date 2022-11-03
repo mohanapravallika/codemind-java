224393d3c1802ca05a957c7f551de0a8f7c44a0b
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-11-03 20:46:46 +0530

    import java.util.*; class UserInputDemo1 { public static void main(String[] args) { Scanner sc= new Scanner(System.in); String str= sc.nextLine(); System.out.print(str); } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;  
+class UserInputDemo1  
+{  
+public static void main(String[] args)  
+{  
+Scanner sc= new Scanner(System.in); 
+String str= sc.nextLine();                
+System.out.print(str);             
+}  
+}  
